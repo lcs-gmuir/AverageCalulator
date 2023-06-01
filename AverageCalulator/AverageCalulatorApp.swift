cd7d48208a02377a605fa9d0c7d05b55c0ecbc6d
griffin muir <gmuir@lcs.on.ca>
2023-06-01 14:05:55 -0400

    Initial Commit

---
+++ AverageCalulator/AverageCalulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AverageCalulatorApp.swift
+//  AverageCalulator
+//
+//  Created by Samantha Stewart on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct AverageCalulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
